6956e30cec12791a86572c57ab2df98bb9fe585e
tehtko <moderndsnsam@gmail.com>
2022-10-04 14:29:02 -0400

    Add models

---
+++ Mangalogue/Entities/Manga.cs
@@ -0,0 +1,12 @@
+﻿namespace Mangalogue.Entities
+{
+    public class Manga
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public ICollection<string>? Pages { get; set; }
+        public ICollection<string>? Genres { get; set; }
+
+        public User Author { get; set; }
+    }
+}
