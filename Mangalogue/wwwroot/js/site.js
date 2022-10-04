5c415ff3936686f76b5ade8ccf7262b33231046d
tehtko <moderndsnsam@gmail.com>
2022-10-04 15:48:03 -0400

    Finish homepage layout

--- Mangalogue/wwwroot/js/site.js
+++ Mangalogue/wwwroot/js/site.js
@@ -1,12 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function scrollToLeft() {
-    console.log("qweqweqw");
-    document.getElementById('new').scrollLeft = -50;
+﻿function scrollToLeft(divToScroll) {
+    document.getElementById(divToScroll).scrollLeft -= 200;
 };
-function scrollToRight() {
-    console.log("qweqweqw");
-    document.getElementById('new').scrollLeft = 50;
+function scrollToRight(divToScroll) {
+    document.getElementById(divToScroll).scrollLeft += 200;
 };
