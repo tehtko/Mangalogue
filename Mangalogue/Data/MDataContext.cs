78e722b1bfe768a99f2ef2d2be8949c6d6e3db1a
tehtko <moderndsnsam@gmail.com>
2022-10-04 14:19:51 -0400

    Add project files.

---
+++ Mangalogue/Data/MDataContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mangalogue.Data
+{
+    public class MDataContext : DbContext
+    { }
+}
---
+++ Mangalogue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mangalogue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
