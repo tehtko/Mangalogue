04b08c23afb9bdb9a51f4ea3ab56813b4e4b4d4e
tehtko <moderndsnsam@gmail.com>
2022-10-04 15:24:36 -0400

    Add horizontal scrolling

--- Mangalogue/wwwroot/js/site.js
+++ Mangalogue/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function scrollToLeft() {
+    console.log("qweqweqw");
+    document.getElementById('new').scrollLeft = -50;
+};
+function scrollToRight() {
+    console.log("qweqweqw");
+    document.getElementById('new').scrollLeft = 50;
+};
