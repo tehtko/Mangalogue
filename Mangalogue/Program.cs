6815b66fea76043330cecdca2b8ff2a458a43e37
tehtko <moderndsnsam@gmail.com>
2022-10-04 14:37:42 -0400

    Update HTTP pipeline

--- Mangalogue/Program.cs
+++ Mangalogue/Program.cs
@@ -14,14 +14,8 @@ builder.Services.AddSingleton<UserService>();
 builder.Services.AddSingleton<MangaService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseExceptionHandler("/Home/Error");
+app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
