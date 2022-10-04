c854d6f09107e2517b938a2a7b52a29932a02a84
tehtko <moderndsnsam@gmail.com>
2022-10-04 14:31:27 -0400

    Add services to the container

--- Mangalogue/Program.cs
+++ Mangalogue/Program.cs
@@ -1,4 +1,5 @@
 using Mangalogue.Data;
+using Mangalogue.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddDbContext<MDataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Mangalogue"));
 });
+builder.Services.AddSingleton<UserService>();
+builder.Services.AddSingleton<MangaService>();
 
 var app = builder.Build();
 
