9d897245c81aa2cc58769e079dbc2b9dba5e5d71
tehtko <moderndsnsam@gmail.com>
2022-10-24 22:02:37 -0400

    Inject MangaService

--- Mangalogue/Services/MangaService.cs
+++ Mangalogue/Services/MangaService.cs
@@ -1,6 +1,16 @@
-﻿namespace Mangalogue.Services
+﻿using Mangalogue.Data;
+using Mangalogue.Entities;
+
+namespace Mangalogue.Services
 {
     public class MangaService
     {
+        private readonly MDataContext _context;
+        public MangaService(MDataContext context)
+        {
+            _context = context;
+        }
+
+        
     }
 }
