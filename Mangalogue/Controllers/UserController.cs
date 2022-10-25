021617ec87a607cf1afc1278bf034cad043662e0
tehtko <moderndsnsam@gmail.com>
2022-10-24 22:02:11 -0400

    Add Login and Signup logic

---
+++ Mangalogue/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using Mangalogue.Entities;
+using Mangalogue.Models;
+using Mangalogue.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mangalogue.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserModel user)
+        {
+            // create a new User entity seperate from the front-end UserModel
+            User _user = new User
+            {
+                Username = user.Username,
+                Email = user.Username,
+                Password = user.Password
+            };
+
+            // check to see if the User exists in the database with matching credentials
+            if (_userService.Login(_user))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            // tell the user that incorrect information was provided
+            else
+            {
+                ViewData["LoginError"] = "Incorrect Username/Email or Password";
+                return View();
+            }
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Signup(UserModel user)
+        {
+            // break out of the method if model state isn't valid
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // create a new User entity seperate from the front-end UserModel
+            User _user = new User
+            {
+                Username = user.Username,
+                Email = user.Email,
+                Password = user.Password
+            };
+
+            // try to add the user and send them back to the homepage
+            if (_userService.AddUser(_user))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            // throw a generic error to the user and send them back to the signup page
+            else
+            {
+                ViewData["SignupError"] = "The username is already taken";
+                return View();
+            }
+        }
+    }
+}
--- Mangalogue/Services/UserService.cs
+++ Mangalogue/Services/UserService.cs
@@ -1,6 +1,51 @@
-﻿namespace Mangalogue.Services
+﻿using Mangalogue.Data;
+using Mangalogue.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mangalogue.Services
 {
     public class UserService
     {
+        private readonly MDataContext _context;
+        public UserService(MDataContext context)
+        {
+            _context = context;
+        }
+
+        internal bool AddUser(User user)
+        {
+            // check to see if the username already exists
+            if (_context.Users.Any(x=>x.Username==user.Username))
+                return false;
+
+            // check to see if the email already exists
+            if (_context.Users.Any(x => x.Email == user.Email))
+                return false;
+
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        internal bool Login(User user)
+        {
+            // check to see if an entity with matching username and password exists
+            if (_context.Users.Any(x => x.Username == user.Username))
+                return true;
+
+            // check to see if an entity with matching email and password exists
+            else if (_context.Users.Any(x => x.Email == user.Email))
+                return true;
+
+            else
+                return false;
+        }
     }
 }
