e25faac2fb19aa44d3c09fd2647b6cb20d94356c
tehtko <moderndsnsam@gmail.com>
2022-10-24 20:23:41 -0400

    Remove constructor

--- Mangalogue/Controllers/HomeController.cs
+++ Mangalogue/Controllers/HomeController.cs
@@ -2,22 +2,13 @@
 using Mangalogue.Models;
 using Mangalogue.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Diagnostics;
 
 namespace Mangalogue.Controllers
 {
     public class HomeController : Controller
     {
-
-        public MangaService _mangaService { get; set; }
-        public UserService _userService { get; set; }
-
-        public HomeController(UserService userService, MangaService mangaService)
-        {
-            _userService = userService;
-            _mangaService = mangaService;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -25,29 +16,12 @@ namespace Mangalogue.Controllers
 
         public IActionResult Manga()
         {
-            List<Chapter> chapters = new();
-            Chapter chapter = new Chapter
-            {
-                Id = 1,
-                Pages = new List<Page>()
-            };
+            return View();
+        }
 
-            chapters.Add(chapter);
-
-            chapter.Pages = new List<Page>();
-            chapter.Pages.Add(new Page { Id = 1, Image = "temp" });
-            chapter.Pages.Add(new Page { Id = 1, Image = "temp" });
-            chapter.Pages.Add(new Page { Id = 1, Image = "temp" });
-            chapter.Pages.Add(new Page { Id = 1, Image = "temp" });
-
-            return View(new Manga
-            {
-                Id = 0,
-                Title = "Demo",
-                Chapters = chapters,
-                Author = new User
-                { }
-            });
+        public IActionResult Login()
+        {
+            return View();
         }
 
         public IActionResult Privacy()
