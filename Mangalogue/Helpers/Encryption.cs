7ebd4866da4609298f29a522ec037e8193bc23d0
tehtko <moderndsnsam@gmail.com>
2022-10-25 18:54:15 -0400

    Rename Hash.cs

--- Mangalogue/Helpers/Encryption.cs
+++ Mangalogue/Helpers/Encryption.cs
@@ -1,18 +1,33 @@
 ﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Mangalogue.Helpers
 {
-    public static class Hash
+    public static class Encryption
     {
         public static string HashPassword(string password)
         {
             // Generate a 128-bit salt using a sequence of
             // cryptographically strong random bytes.
             byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
-            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
 
             // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password!,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+
+        public static string UnhashPassword(string password, string s)
+        {
+            byte[] salt = Encoding.ASCII.GetBytes(s); // convert the salt into a byte array
+
+            // same implementation as above
             string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
                 password: password!,
                 salt: salt,
