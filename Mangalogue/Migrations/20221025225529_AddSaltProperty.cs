447913c9837befaf5447bfae57fb8c609b9cf0c2
tehtko <moderndsnsam@gmail.com>
2022-10-25 18:59:01 -0400

    Add salt column

---
+++ Mangalogue/Migrations/20221025225529_AddSaltProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mangalogue.Migrations
+{
+    public partial class AddSaltProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "salt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "salt",
+                table: "Users");
+        }
+    }
+}
