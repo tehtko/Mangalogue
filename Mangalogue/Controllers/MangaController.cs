30fbd71919944c50b84410eba04ecbc7363be137
tehtko <moderndsnsam@gmail.com>
2022-10-24 20:23:36 -0400

    Create HomeController

---
+++ Mangalogue/Controllers/MangaController.cs
@@ -0,0 +1,30 @@
+﻿using Mangalogue.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mangalogue.Controllers
+{
+    public class MangaController : Controller
+    {
+        public MangaService _mangaService { get; set; }
+
+        public MangaController(MangaService mangaService)
+        {
+            _mangaService = mangaService;
+        }
+
+        public IActionResult Login()
+        {
+            return View("Account");
+        }
+
+        public IActionResult Signup()
+        {
+            return View("Account");
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
