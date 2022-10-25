78333009bc8e23acf3a7969b87b2724c4aa9dfbf
tehtko <moderndsnsam@gmail.com>
2022-10-25 18:58:28 -0400

    Add session and memorycache

--- Mangalogue/Program.cs
+++ Mangalogue/Program.cs
@@ -6,12 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<MDataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Mangalogue"));
 }, ServiceLifetime.Scoped);
+
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<MangaService>();
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = false;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -20,6 +29,7 @@ if(!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
