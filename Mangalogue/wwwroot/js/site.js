59fc50277990117629f3efb0ab17704af51e4d75
tehtko <moderndsnsam@gmail.com>
2022-10-27 01:31:13 -0400

    Fade page on sidebar open

--- Mangalogue/wwwroot/js/site.js
+++ Mangalogue/wwwroot/js/site.js
@@ -9,13 +9,20 @@ function scrollToRight(divToScroll) {
 
 function toggleNav() {
     if (state == "closed") {
-        document.getElementById("content-sidebar").style.width = "200px";
+        document.getElementById("content-sidebar").style.width = "220px";
+        document.getElementById("page-body").style.opacity = "0.5";
+        document.getElementById("front-page").style.opacity = "0.5";
         state = "opened"
         return;
     }
 
     if (state == "opened") {
-        document.getElementById("content-sidebar").style.width = "0px";
+        document.getElementById("content-sidebar").style.width = "0";
+        setTimeout(function () {
+            document.getElementById("page-body").style.opacity = "1";
+            document.getElementById("front-page").style.opacity = "1";
+        }, 200);
+        
         state = "closed"
         return;
     }
