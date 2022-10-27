2030b87f2bd18e15ac5599960ab5a862da83d516
tehtko <moderndsnsam@gmail.com>
2022-10-27 19:57:27 -0400

    Update entities

--- Mangalogue/Entities/Manga.cs
+++ Mangalogue/Entities/Manga.cs
@@ -7,6 +7,7 @@ namespace Mangalogue.Entities
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Cover { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public ICollection<Chapter>? Chapters { get; set; }
 
         public User Author { get; set; }
