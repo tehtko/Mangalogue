d52b2ce202ddbff8003c6e15e460e0d53b0d4ef9
tehtko <moderndsnsam@gmail.com>
2022-10-27 19:58:11 -0400

    Create MangaViewModel

---
+++ Mangalogue/Models/MangaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Mangalogue.Entities;
+
+namespace Mangalogue.Models
+{
+    public class MangaViewModel
+    {
+        public ICollection<Manga>? Newest { get; set; }
+        public ICollection<Manga>? MostPopular { get; set; }
+        public ICollection<Manga>? Trending { get; set; }
+    }
+}
