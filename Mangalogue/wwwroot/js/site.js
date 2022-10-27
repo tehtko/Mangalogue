4f4d49786310d417fa7bc1117b6d4c7a1ac18bab
tehtko <moderndsnsam@gmail.com>
2022-10-27 00:07:55 -0400

    Sliding navbar

--- Mangalogue/wwwroot/js/site.js
+++ Mangalogue/wwwroot/js/site.js
@@ -1,6 +1,30 @@
-﻿function scrollToLeft(divToScroll) {
+﻿var state = "closed";
+function scrollToLeft(divToScroll) {
     document.getElementById(divToScroll).scrollLeft -= 200;
 };
 function scrollToRight(divToScroll) {
     document.getElementById(divToScroll).scrollLeft += 200;
 };
+
+
+function toggleNav() {
+    if (state == "closed") {
+        document.getElementById("content-sidebar").style.width = "200px";
+        state = "opened"
+        return;
+    }
+
+    if (state == "opened") {
+        document.getElementById("content-sidebar").style.width = "0px";
+        state = "closed"
+        return;
+    }
+}
+
+$(document).ready(function () {
+    $('.navbar-light .dmenu').hover(function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+    }, function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+    });
+});
