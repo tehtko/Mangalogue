93ba2ef099dc8ccc8ed902404117f70ef3c04125
tehtko <moderndsnsam@gmail.com>
2022-10-28 22:10:17 -0400

    Comments

--- Mangalogue/Helpers/ImageConverter.cs
+++ Mangalogue/Helpers/ImageConverter.cs
@@ -7,23 +7,37 @@ namespace Mangalogue.Helpers
     {
         public static byte[] ConvertToBinary(IFormFile image, IWebHostEnvironment _webHostEnvironment)
         {
+            // get the path to store the image temporarily
             string _path = Path.Combine(_webHostEnvironment.WebRootPath, "Pages");
+
+            // get the full file name and path
             string fileNameWithPath = Path.Combine(_path, image.FileName);
 
+            // make sure a file with the same name doesn't already exist
+            if (File.Exists(fileNameWithPath))
+            {
+                File.Delete(fileNameWithPath);
+            }
+
+            // save the image so we can read its data
             using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
             {
                 image.CopyTo(stream);
             }
 
+            // create a byte array with the images binary data
             byte[] data = File.ReadAllBytes(fileNameWithPath);
 
+            // delete the image
             File.Delete(fileNameWithPath);
 
+            // return the images data to be stored in the database
             return data;
         }
 
         public static void ConvertToImage(string path, byte[] image)
         {
+            // dump the binary data into a specified file
             File.WriteAllBytes(path, image);
         }
     }
