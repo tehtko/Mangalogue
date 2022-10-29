e35eb05d4da73690bc6e8e25dee55b3aa1e0bb15
tehtko <moderndsnsam@gmail.com>
2022-10-28 22:02:34 -0400

    Create Helper class to convert images to and from binary

---
+++ Mangalogue/Helpers/ImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Mangalogue.Helpers
+{
+    public class ImageConverter
+    {
+        public static byte[] ConvertToBinary(IFormFile image, IWebHostEnvironment _webHostEnvironment)
+        {
+            string _path = Path.Combine(_webHostEnvironment.WebRootPath, "Pages");
+            string fileNameWithPath = Path.Combine(_path, image.FileName);
+
+            using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+
+            byte[] data = File.ReadAllBytes(fileNameWithPath);
+
+            System.IO.File.Delete(fileNameWithPath);
+
+            return data;
+        }
+
+        public static void ConvertToImage(string path, byte[] image)
+        {
+            System.IO.File.WriteAllBytes(path, image);
+        }
+    }
+}
