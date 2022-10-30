f721b92b318fc91d5af0eb9684f34b18260e2df9
tehtko <moderndsnsam@gmail.com>
2022-10-30 18:21:32 -0400

    No longer save the physical file

--- Mangalogue/Helpers/ImageConverter.cs
+++ Mangalogue/Helpers/ImageConverter.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System.IO;
 
 namespace Mangalogue.Helpers
 {
@@ -7,32 +9,11 @@ namespace Mangalogue.Helpers
     {
         public static byte[] ConvertToBinary(IFormFile image, IWebHostEnvironment _webHostEnvironment)
         {
-            // get the path to store the image temporarily
-            string _path = Path.Combine(_webHostEnvironment.WebRootPath, "Pages");
-
-            // get the full file name and path
-            string fileNameWithPath = Path.Combine(_path, image.FileName);
-
-            // make sure a file with the same name doesn't already exist
-            if (File.Exists(fileNameWithPath))
+            using (var memoryStream = new MemoryStream())
             {
-                File.Delete(fileNameWithPath);
+                image.CopyTo(memoryStream);
+                return memoryStream.ToArray();
             }
-
-            // save the image so we can read its data
-            using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
-            {
-                image.CopyTo(stream);
-            }
-
-            // create a byte array with the images binary data
-            byte[] data = File.ReadAllBytes(fileNameWithPath);
-
-            // delete the image
-            File.Delete(fileNameWithPath);
-
-            // return the images data to be stored in the database
-            return data;
         }
 
         public static void ConvertToImage(string path, byte[] image)
