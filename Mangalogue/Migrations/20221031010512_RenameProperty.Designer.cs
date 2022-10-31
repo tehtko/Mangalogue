46e1d1de6a99850871f0b1caae7be258763eb0a2
tehtko <moderndsnsam@gmail.com>
2022-10-30 21:23:09 -0400

    Add Migrations

--- Mangalogue/Migrations/20221031010512_RenameProperty.Designer.cs
+++ Mangalogue/Migrations/20221031010512_RenameProperty.Designer.cs
@@ -4,6 +4,7 @@ using Mangalogue.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mangalogue.Migrations
 {
     [DbContext(typeof(MDataContext))]
-    partial class MDataContextModelSnapshot : ModelSnapshot
+    [Migration("20221031010512_RenameProperty")]
+    partial class RenameProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mangalogue/Migrations/20221031010512_RenameProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mangalogue.Migrations
+{
+    public partial class RenameProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cover",
+                table: "Mangas",
+                newName: "Thumbnail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Thumbnail",
+                table: "Mangas",
+                newName: "Cover");
+        }
+    }
+}
