be6cef7cdb5256c8e1955bf8550a232540f4bd2f
tehtko <moderndsnsam@gmail.com>
2022-10-30 21:23:23 -0400

    Change Thumbnail property name

--- Mangalogue/Models/MangaCreationViewModel.cs
+++ Mangalogue/Models/MangaCreationViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using Mangalogue.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mangalogue.Models
 {
     public class MangaCreationViewModel
     {
         public string? Title { get; set; }
-        public IFormFile? CoverImage { get; set; }
+        public IFormFile? Thumbnail { get; set; }
         public string? Description { get; set; }
+        [BindProperty]
         public ICollection<Genres>? Genres { get; set; }
     }
 }
