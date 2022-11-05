77f61763b7e96f98105c34515d0c7efda92f82ff
tehtko <moderndsnsam@gmail.com>
2022-11-04 20:51:55 -0400

    Use attribute

--- Mangalogue/Models/MangaCreationViewModel.cs
+++ Mangalogue/Models/MangaCreationViewModel.cs
@@ -1,14 +1,22 @@
 ﻿using Mangalogue.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mangalogue.Models
 {
     public class MangaCreationViewModel
     {
+        [Required(ErrorMessage = "Please add a title")]
         public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Please add a thumbnail")]
         public IFormFile? Thumbnail { get; set; }
+
+        [Required(ErrorMessage = "Please add brief description")]
         public string? Description { get; set; }
+
         [BindProperty]
+        [CheckBoxRequired(ErrorMessage = "Please select at least one genre")]
         public ICollection<Genres>? Genres { get; set; }
     }
 }
