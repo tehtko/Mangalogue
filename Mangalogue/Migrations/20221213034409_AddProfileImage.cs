3dbbb6407934f311e6f94a9165cd6d52f18ec737
tehtko <moderndsnsam@gmail.com>
2022-12-13 00:26:19 -0500

    Add Date and profile image column

---
+++ Mangalogue/Migrations/20221213034409_AddProfileImage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mangalogue.Migrations
+{
+    public partial class AddProfileImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfileImage",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "Users");
+        }
+    }
+}
