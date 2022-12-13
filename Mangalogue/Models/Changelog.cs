0553df9bae4e2368a314d280a98a71033e6af80f
tehtko <moderndsnsam@gmail.com>
2022-12-12 21:09:30 -0500

    Create changelog model

---
+++ Mangalogue/Models/Changelog.cs
@@ -0,0 +1,10 @@
+﻿namespace Mangalogue.Models
+{
+    public class Changelog
+    {
+        public int Id { get; set; }
+        public DateOnly PostDate { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
