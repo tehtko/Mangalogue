a9cbc908ca041a912eb6a93ae2a5c6c510d047ca
tehtko <davidgordon12@live.ca>
2022-12-22 19:50:35 -0500

    Remove ChapterCount property

--- Mangalogue/Models/ChapterUploadViewModel.cs
+++ Mangalogue/Models/ChapterUploadViewModel.cs
@@ -5,7 +5,6 @@ namespace Mangalogue.Models
     public class ChapterUploadViewModel
     {
         public Manga Manga { get; set; }
-        public int ChapterNumber { get; set; }
         public ICollection<IFormFile>? Pages { get; set; }
     }
 }
